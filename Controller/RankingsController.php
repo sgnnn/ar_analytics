1a7b828c7cee0e6b530167373ecec1409114e21b
sgnnn <sgnnn>
2015-09-30 18:57:22 +0900

    ランキングすこし着手

--- Controller/RankingsController.php
+++ Controller/RankingsController.php
@@ -10,7 +10,19 @@ class RankingsController extends AppController {
 		$this->layout = "Normal";
 		$this->display = "Rankings";
 
+		if(isset($this->params['url']["category"]))
+			$category = $this->params['url']["category"];
+		else
+			$category = "count";
+
+		if(isset($this->params['url']["racerRank"]))
+			$racerRank = $this->params['url']["racerRank"];
+		else
+			$racerRank = "all";
+
 		$this->set("display",$this->display);
+		$this->set("category", $category);
+		$this->set("racerRank", $racerRank);
 	}
 
 	/****************************************************
---
+++ View/Rankings/index.ctp
@@ -0,0 +1,19 @@
+<input type="hidden" id="category" value="<?php echo $category; ?>" >
+<input type="hidden" id="racerRank" value="<?php echo $racerRank; ?>" >
+
+<div class="category_select">
+	<a href="#">勝利数</a>
+	<a href="#">勝率</a>
+	<a href="#">優勝回数</a>
+</div>
+
+<div class="racer_rank_select">
+	<a href="#">すべて</a>
+	<a href="#">S級</a>
+	<a href="#">A級</a>
+	<a href="#">B級</a>
+</div>
+
+<div>
+
+</div>
